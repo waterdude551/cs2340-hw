e944387eb1c2fc3e2c4613017f5910b2c8ce41ed
waterdude551 <waterdude551@gmail.com>
2025-08-27 14:27:07 -0400

    finished chapter 5

--- moviesstore/home/views.py
+++ moviesstore/home/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    template_data = {}
 
-    return render(request, 'home/index.html')
+    template_data['title'] = 'Movies Store'
+
+    return render(request, 
+                  'home/index.html',
+                  {'template_data' : template_data})
 
 def about(request):
 
-    return render(request, 'home/about.html')
+    template_data = {}
+
+    template_data['title'] = 'About'
+
+    return render(request, 
+                  'home/about.html', 
+                  {'template_data' : template_data})
